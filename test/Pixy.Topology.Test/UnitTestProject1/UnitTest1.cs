ae3e63145a45688e434337950a43991c1cddb686
俞俞 <hanabiranoshitaniumore@gmail.com>
2018-09-29 02:36:56 +0800

    +appveyor.yml

---
+++ test/Pixy.Topology.Test/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+
+
+namespace Pixy.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UnitTestSetup
+    {
+        [Test]
+        public void Check()
+        {
+            Assert.Pass();
+        }
+    }
+}
